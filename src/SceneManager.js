8e057e7d1b4bb7f43c9345244c0503519fb1d4fd
Robert Reutiman <rreutima@nd.edu>
2021-05-10 21:57:19 -0400

    Instanced cube

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -1,8 +1,10 @@
 // Global Constants
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
-const cube_geometry = new THREE.BoxGeometry();
+const camera = new THREE.PerspectiveCamera( 60, window.innerWidth / window.innerHeight, 0.1, 100 );
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+const color = new THREE.Color();
+const raycaster = new THREE.Raycaster();
+const mouse = new THREE.Vector2( 1, 1 );
 
 // Global Variables
 var golMatrix;
@@ -11,56 +13,118 @@ var controls;
 var gui;
 var gui_props;
 var spotlight;
+var mesh;
 
 window.onload = function init() {
   // Set up renderer
-  renderer.setSize(  2400, 1800 );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.setClearColor( 0xffffff, 0);
   document.body.appendChild( renderer.domElement );
-  gui_props = {light_posX:5}
+  gui_props = { light_posX: 5}
+
   // Set background
   scene.background = new THREE.TextureLoader().load("../textures/space.jpeg");
 
   // Set camera
-  camera.position.set(0, 0, 5);
+  camera.position.set(5, 15, 15);
+  camera.lookAt( 0, 0, 0 );
   controls = new THREE.OrbitControls(camera, renderer.domElement);
-  // Initialize materials
-  colors = [
-    0xffffff,
-    0x8bedd9,
-    0xa58bed,
-    0xed8bb1,
-    0xeddb8b
-  ]
-  for(color of colors) {
-    materials.push(new THREE.MeshPhongMaterial( { color: color, specular: 0xffffff, shininess: 10, flatShading: false } ));
-  }
+
+  // Initialize geometry and material
+  const geometry = new THREE.BoxGeometry();
+  const material = new THREE.MeshPhongMaterial(); //{opacity: 0.8, transparent: true});
 
   // Initialize golMatrix to 3D matrix to represent entries
-  // 100 x 100 x 100
-  // We can add more potentially as a buffer area where cells slowly fade (gliders and such)
+  // We will need an instanced material to change transparency
+  mesh = new THREE.InstancedMesh( geometry, material, 1000 );
+
+  let inc = 0;
+  const offset = -1.5;
+
+  const matrix = new THREE.Matrix4();
+
   golMatrix = [];
-  for(let i=0; i < 5; i+=1) {
+  for(let i=0; i < 10; i+=1) {
     golMatrix[i] = [];
-    for(let j=0; j < 5; j+=1) {
+    for(let j=0; j < 10; j+=1) {
       golMatrix[i][j] = [];
-      for(let k=0; k < 5; k+=1) {
-        // we will need to change the way we grab materials upon scaling up size of matrix
-        // using 5 x 5 x 5 for testing
-        golMatrix[i][j][k] = new Cell(i, j, k, materials[k], true);
+      for(let k=0; k < 10; k+=1) {
+
+        golMatrix[i][j][k] = new Cell(true);
+
+        // Create actual cells
+        matrix.setPosition( offset * i, offset * j, offset * k );
+  
+        mesh.setMatrixAt( inc, matrix );
+        mesh.setColorAt( inc, color );
+  
+        inc++;
       }
     }
   }
 
+  scene.add( mesh );
+  
   // Add lighting
-  const light = new THREE.AmbientLight(0xffffff, 0.1);
-  scene.add(light);
+  //const light = new THREE.AmbientLight(0xffffff, 0.1);
+  //scene.add(light);
   spotlight = new THREE.SpotLight(0xffffff, 1.5);
-  spotlight.position.set(5,0,2);
+  spotlight.position.set(-10, 10, 10);
   spotlight.castShadow = true;
   scene.add(spotlight);
   gui = new dat.GUI({height: 5*32 - 1});
   gui.add(gui_props, "light_posX", 0, 10);
+
+  // const light1 = new THREE.HemisphereLight( 0xffffff, 0x000088 );
+  // light1.position.set( - 1, 1.5, 1 );
+  // scene.add( light1 );
+
+  // const light2 = new THREE.HemisphereLight( 0xffffff, 0x880000, 0.5 );
+  // light2.position.set( - 1, - 1.5, - 1 );
+  // scene.add( light2 );
+
+  window.addEventListener( 'resize', onWindowResize );
+  document.addEventListener( 'mousemove', onMouseMove );
+  document.addEventListener( 'click', onClick );
+
+
   animate();
+}
+
+function onWindowResize() {
+
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function onMouseMove( event ) {
+
+  event.preventDefault();
+
+  mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+  mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+}
+
+function onClick() {
+  raycaster.setFromCamera( mouse, camera );
+
+  let intersection = raycaster.intersectObject( mesh, true);
+
+  console.dir(intersection);
+
+  if ( intersection.length > 0 ) {
+
+    const instanceId = intersection[0].instanceId;
+
+    mesh.setColorAt( instanceId, color.setHex(0x000000) );
+    mesh.instanceColor.needsUpdate = true;
+
+  }
 }
 
 function gameOfLife() {
@@ -70,13 +134,14 @@ function gameOfLife() {
 function animate() {
 	requestAnimationFrame( animate );
 
-  for(let i=0; i < 5; i+=1) {
-    for(let j=0; j < 5; j+=1) {
-      for(let k=0; k < 5; k+=1) {
-        golMatrix[i][j][k].draw();
-      }
-    }
-  }
+  // for(let i=0; i < 10; i+=1) {
+  //   for(let j=0; j < 10; j+=1) {
+  //     for(let k=0; k < 10; k+=1) {
+  //
+  //     }
+  //   }
+  // }
+
   spotlight.position.x = gui_props.light_posX;
   controls.update();
 	renderer.render( scene, camera );
@@ -86,25 +151,10 @@ function animate() {
 //  Cell Class
 //  Represents a cell within the 3D grid
 //  parameters:
-//    int x, y, z
-//    material material
 //    boolean alive
 //
 class Cell {
-  constructor(x, y, z, material, alive) {
-
-    var geometry = cube_geometry;
-    // TODO: fix geometry so cubes render in proper locations
-    // use x, y, z to calculate proper position
-    //geometry = geometry.translate( 0.0125 * x, 0.0125 * y, 0.0125 * z);
-    var cube = new THREE.Mesh( geometry, material );
-    cube.receiveShadow = true;
-
-    this.cube = cube;
+  constructor(alive) {
     this.alive = alive;
-  }
-
-  draw() {
-    if( this.alive ) scene.add( this.cube );
   }
 }
